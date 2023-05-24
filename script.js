517756b54933b034db6acb43aa12aea8aeba9998
9ut4 <9ut4@fresnou.org>
2023-05-24 14:20:48 -0700

    Added More Text to shop, Hides items when not unlocked yet | 05-24-2023

--- script.js
+++ script.js
@@ -7,39 +7,74 @@ const shopClose = document.getElementById('shopClose');
 let counter = document.getElementById('counter');
 let TC = document.getElementById('Totalcounter');
 const reset = document.getElementById('reset');
+let CalText = document.getElementById('CalText')
 
 //Values and Upgrades
 let clicks = 0;
 let totalClicks = 0;
+const DubItem = document.getElementById('DubItem')
+const TenItem = document.getElementById('TenItem');
+const DubButton = document.getElementById('BobbleHead');
 const TenButton = document.getElementById('Balloon');
-const DubButton = document.getElementById('BobbleHead')
 let Dub = 0;
 let Ten = 0;
 let DubPrice = 20;
 let TenPrice = 100;
 
 //Main Functions
+if (totalClicks < 15){
+    DubItem.classList.remove('item');
+    DubItem.classList.add('hidden');
+    TenItem.classList.remove('item');
+    TenItem.classList.add('hidden');
+}
+
 function clicker (){
     clicks = clicks + 1 + Dub + Ten;
     totalClicks = totalClicks + 1 + Dub + Ten;
+    ShopText();
+    if (totalClicks < 15){
+        DubItem.classList.remove('item');
+        DubItem.classList.add('hidden');
+    } else if (totalClicks >= 15){
+        DubItem.classList.remove('hidden');
+        DubItem.classList.add('item');
+    }
+    if (totalClicks < 75){
+        TenItem.classList.remove('item');
+        TenItem.classList.add('hidden');
+    } else if (totalClicks >= 75){
+        TenItem.classList.remove('hidden');
+        TenItem.classList.add('item');
+    }
+    if (totalClicks < 15){
+        DubItem.classList.remove('item');
+        DubItem.classList.add('hidden');
+        TenItem.classList.remove('item');
+        TenItem.classList.add('hidden');
+    }
 }
 
 function gameReset(){
     clicks = 0;
     totalClicks = 0;
-    upgradeDub = false;
-    upgradeTen = false;
+    Dub = 0;
+    Ten = 0;
     DubPrice = 20;
     TenPrice = 100;
-    counter.innerHTML= `Click to Start`;
-    TC.classList.add('hidden');
+    counter.innerHTML= `Welcome`;
+    TC.innerHTML= `Click Golden Egg to Start`;
     picture.classList.remove('hidden');
     picture.src='https://cdn.wikimg.net/en/splatoonwiki/images/c/cf/S2_Icon_Golden_Egg.png';
     endScreen.classList.add('hidden');
+    shop.classList.add('hidden');
+    shopButton.classList.remove('hidden');
+    shopClose.classList.add('hidden')
 }
 function gameEnd(){
     clicks = 0;
     counter.innerHTML= `Thanks for playing!`;
+    TC.innerHTML= `${totalClicks} Power Eggs Generated`
     picture.classList.add('hidden');
     endScreen.classList.remove('hidden');
 }
@@ -54,25 +89,41 @@ function salmonIcon() {
          picture.src = 'https://cdn.wikimg.net/en/splatoonwiki/images/4/43/S3_Smallfry_icon.png';
      }
 }
+function ShopText(){
+    if (totalClicks >= 20 && Dub >= 2){
+        CalText.innerHTML= 'Welcomes to the shop! Buy some upgrades for ' +
+            'Salmon here!'
+    }else if(totalClicks >= 15 ){
+        CalText.innerHTML= 'Look at what I have in store for you!'
+    }
+    if (totalClicks >= 75 && Dub >= 10){
+        CalText.innerHTML= 'Welcomes to the shop! Buy some upgrades for ' +
+            'Salmon here!'
+    }
+}
 
 //Shop
 function shopStart(){
+    ShopText();
     DubButton.addEventListener('click', function(){
         if (DubPrice <= 60){
             if (clicks >= DubPrice){
                 Dub = Dub + 2;
                 clicks = clicks - DubPrice;
                 DubPrice = DubPrice + 4;
-                DubButton.innerHTML= `Purchase for ${DubPrice}`
+                DubButton.innerHTML= `Purchase for ${DubPrice}`;
                 counter.innerHTML= `${clicks} Power Eggs`;
-                TC.innerHTML= `${totalClicks} All Time Power Eggs`
+                TC.innerHTML= `${totalClicks} All Time Power Eggs`;
+                CalText.innerHTML= 'Thank you for purchasing!';
+            } else {
+                CalText.innerHTML= `That's not enough...?`;
             }
         } else if (DubPrice >= 65){
             DubButton.innerHTML= `Max`
         }
     })
     TenButton.addEventListener('click', function(){
-        if (TenPrice <= 500){
+        if (TenPrice < 500){
             if (clicks >= TenPrice){
                 Ten = Ten + 10;
                 clicks = clicks - TenPrice;
@@ -100,11 +151,11 @@ shopClose.addEventListener('click', function(){
     shopButton.classList.remove('hidden');
     shopClose.classList.add('hidden')
 })
+
 picture.addEventListener('click', function(){
     clicker();
     salmonIcon();
     counter.innerHTML= `${clicks} Power Eggs`;
-    TC.classList.remove('hidden');
     TC.innerHTML= `${totalClicks} All Time Power Eggs`
     if (clicks >= 10000){
         gameEnd();
