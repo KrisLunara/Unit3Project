74b8e27ef10643557de7a37097774d9c23aa09e7
9ut4 <9ut4@fresnou.org>
2023-05-25 14:17:51 -0700

    Finished all shop text, added a few Easter Eggs | 05-25-2023

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+console.log(`Marie: What are you doing here? This is for the dev
+to check to make sure something is working correctly.`);
+console.log(`Marie: Even though I would, don't cheat the
+game. Callie is happy to give you some of the junk she
+bought at Hotlantis.`);
+
 //Asset Linking
 const picture = document.getElementById('object');
 const endScreen = document.getElementById('endScreen');
@@ -7,7 +13,9 @@ const shopClose = document.getElementById('shopClose');
 let counter = document.getElementById('counter');
 let TC = document.getElementById('Totalcounter');
 const reset = document.getElementById('reset');
-let CalText = document.getElementById('CalText')
+let CalText = document.getElementById('CalText');
+let CalIcon = document.getElementById('CalIcon');
+const Title = document.getElementById('title');
 
 //Values and Upgrades
 let clicks = 0;
@@ -20,6 +28,8 @@ let Dub = 0;
 let Ten = 0;
 let DubPrice = 20;
 let TenPrice = 100;
+
+let KingSalmon = Math.floor(Math.random() * 10);
 
 //Main Functions
 if (totalClicks < 15){
@@ -70,6 +80,8 @@ function gameReset(){
     shop.classList.add('hidden');
     shopButton.classList.remove('hidden');
     shopClose.classList.add('hidden')
+    CalIcon.src='https://cdn.wikimg.net/en/splatoonwiki/images/4/4c/S3_Icon_Callie.png';
+    Title.innerHTML= `Salmonid Clicker`;
 }
 function gameEnd(){
     clicks = 0;
@@ -80,7 +92,13 @@ function gameEnd(){
 }
 function salmonIcon() {
     if(totalClicks >= 500){
-        picture.src = 'https://cdn.wikimg.net/en/splatoonwiki/images/7/7a/S3_Cohozuna_icon.png';
+        switch(KingSalmon){
+            case 0:
+                picture.src='https://cdn.wikimg.net/en/splatoonwiki/images/0/0a/S3_Horrorboros_icon.png';
+                break
+            default:
+                picture.src='https://cdn.wikimg.net/en/splatoonwiki/images/7/7a/S3_Cohozuna_icon.png';
+        }
     }else if (totalClicks >= 100){
         picture.src = 'https://cdn.wikimg.net/en/splatoonwiki/images/3/3d/S3_Cohock_icon.png';
     } else if(totalClicks >= 50){
@@ -90,15 +108,23 @@ function salmonIcon() {
      }
 }
 function ShopText(){
-    if (totalClicks >= 20 && Dub >= 2){
+    if (totalClicks >= 20 && totalClicks <75 && Dub >= 2){
         CalText.innerHTML= 'Welcomes to the shop! Buy some upgrades for ' +
-            'Salmon here!'
-    }else if(totalClicks >= 15 ){
-        CalText.innerHTML= 'Look at what I have in store for you!'
+            'your Salmon here!';
+    }else if(totalClicks >= 15 && totalClicks < 74 && Dub === 0){
+        CalText.innerHTML= 'Look at what I have in store for you!';
     }
     if (totalClicks >= 75 && Dub >= 10){
         CalText.innerHTML= 'Welcomes to the shop! Buy some upgrades for ' +
-            'Salmon here!'
+            'Salmon here!';
+    }else if (totalClicks >= 75 && Ten === 0){
+        CalText.innerHTML= 'Look at this new item I have for you!';
+    }else if (totalClicks < 75 && totalClicks >=50  && Dub >= 2 && Ten === 0){
+        CalText.innerHTML= 'I can offer you a new upgrade if you reach 75 ' +
+            'power eggs total!';
+    } else if (Dub >= 2 && Ten >=10){
+        CalText.innerHTML= 'Welcome to the shop! Buy some more items' +
+            ' for your Salmon here!';
     }
 }
 
@@ -115,11 +141,12 @@ function shopStart(){
                 counter.innerHTML= `${clicks} Power Eggs`;
                 TC.innerHTML= `${totalClicks} All Time Power Eggs`;
                 CalText.innerHTML= 'Thank you for purchasing!';
-            } else {
+            } else if (clicks < DubPrice){
                 CalText.innerHTML= `That's not enough...?`;
             }
-        } else if (DubPrice >= 65){
+        } else if (DubPrice >= 64){
             DubButton.innerHTML= `Max`
+            CalText.innerHTML= `Sorry. I ran out of those.`;
         }
     })
     TenButton.addEventListener('click', function(){
@@ -131,9 +158,13 @@ function shopStart(){
                 TenButton.innerHTML= `Purchase for ${TenPrice}`
                 counter.innerHTML= `${clicks} Power Eggs`;
                 TC.innerHTML= `${totalClicks} All Time Power Eggs`
+                CalText.innerHTML= 'Thank you for purchasing!';
+            }else if (clicks < TenPrice){
+                CalText.innerHTML= `That's not enough...?`;
             }
         } else if (TenPrice >= 500){
-            TenButton.innerHTML= `Max`
+            DubButton.innerHTML= `Max`
+            CalText.innerHTML= `Sorry. I ran out of those.`;
         }
     })
 }
@@ -150,6 +181,25 @@ shopClose.addEventListener('click', function(){
     shop.classList.add('hidden');
     shopButton.classList.remove('hidden');
     shopClose.classList.add('hidden')
+})
+
+CalIcon.addEventListener('click', function(){
+    let random = Math.floor(Math.random() * 3)
+    switch (random){
+        case 0:
+            CalIcon.src='https://cdn.wikimg.net/en/splatoonwiki/images/3/30/S2_Icon_Callie_2.png';
+            break
+        case 1:
+            CalIcon.src='https://cdn.wikimg.net/en/splatoonwiki/images/6/6d/S2_Icon_Callie_3.png';
+            break
+        case 2:
+            CalIcon.src='https://cdn.wikimg.net/en/splatoonwiki/images/e/e3/S2_Icon_Callie.png';
+            break
+    }
+
+})
+Title.addEventListener('click', function (){
+    Title.innerHTML= `Fishy Clicker XD`;
 })
 
 picture.addEventListener('click', function(){
